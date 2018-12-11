2498994bdf4551c1de8554e3251fdb1a72969cf2
umkin <naum.oleks@gmail.com>
2018-12-11 23:40:55 +0200

    1/ test compound for 1.3.js is changed 2/ reinstalled jest test package

--- 1.2.js
+++ 1.2.js
@@ -1,4 +1,3 @@
 const result = () => (5 + 4 + (2 - (3 - (6 + (4 / 5))))) / (3 * (6 - 2) * (2 - 7));
 
 module.exports = result;
-console.log(result());
--- tests/1.2.test.js
+++ tests/1.2.test.js
@@ -1,9 +1,6 @@
 const result = require('../1.2.js');
 
 test('show result -0.2....', () => {
-  expect(result().toBe(-0.24666666666666667));
-});
-
-test('is "-0.24666666666666667" equal to "result"?', () => {
-  expect((result() === -0.24666666666666667).toBe(true));
+  expect(result()).toBe(-0.24666666666666667);
+  expect(result() === -0.24666666666666667).toBe(true);
 });
